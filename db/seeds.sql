16042e73e7667bd4e229e430fdca77384f8a6afc
jponly <jpangeli514@gmail.com>
2018-09-15 18:39:58 -0700

    jaws db add

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
--- INSERT INTO burger (name, devoured) VALUES ('Cheeseburger', true);
--- INSERT INTO burger (name) VALUES ('Swiss Cheddar');
--- INSERT INTO burger (name) VALUES ('Ultimate Bacon');
--- INSERT INTO burger (name, devoured) VALUES ('Southwest Onion Ring', true);
--- INSERT INTO burger (name) VALUES ('Jethro Burger');
--- INSERT INTO burger (name, devoured) VALUES ('Whiplash Veggie', true);
+INSERT INTO burger (name, devoured) VALUES ('Cheeseburger', true);
+INSERT INTO burger (name) VALUES ('Swiss Cheddar');
+INSERT INTO burger (name) VALUES ('Ultimate Bacon');
+INSERT INTO burger (name, devoured) VALUES ('Southwest Onion Ring', true);
+INSERT INTO burger (name) VALUES ('Jethro Burger');
+INSERT INTO burger (name, devoured) VALUES ('Whiplash Veggie', true);
